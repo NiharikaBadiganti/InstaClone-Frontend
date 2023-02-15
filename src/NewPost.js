98b378252b88814242dd64dc5ded5ed58decdac7
NiharikaBadiganti <badigantiniharika@gmail.com>
2023-02-15 08:03:34 +0530

    Changes in Frontend

--- src/NewPost.js
+++ src/NewPost.js
@@ -30,7 +30,7 @@ export const NewPost = () =>
         //         navigate('/ViewPost')
         //     }).catch(error => console.log(error))
         
-        await fetch("https://instaclone-project-6pt0.onrender.com/NewPost", {
+        await fetch("https://instaclone-project-6pt0.onrender.com/newpost", {
             method: "post",
             body: formData,
             redirect: "follow"
